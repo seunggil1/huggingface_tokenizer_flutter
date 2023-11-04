a792acb9d0002f2cf073db8835708919d0ac9e22
seung <ksgg1navercom@gmail.com>
2023-11-05 03:28:12 +0900

    [Rust] rust init2

--- README.md
+++ README.md
@@ -8,7 +8,16 @@
 ## INSTALL
 1. visual studio 설치(vscode X)
 2. rust 설치 : https://www.rust-lang.org/tools/install
+3. Android Studio 설치
+4. Android Studio > Preferences > Appearance & Behaviour > Android SDK > SDK Tools : 아래 4개 설치
+```
+* Android SDK Tools
+* NDK
+* CMake
+* LLDB
+```
 
+5. Rust 설정
 ```shell
 rustup target add aarch64-linux-android
 rustup target add armv7-linux-androideabi
--- rust_lib/src/lib.rs
+++ rust_lib/src/lib.rs
@@ -1,5 +1,18 @@
+use tokenizers::tokenizer::{Result, Tokenizer};
+
 pub fn greet(to: String) -> String {
     format!("Hello, {}", to)
+}
+
+
+fn encode() -> Result<()> {
+    // let tokenizer = Tokenizer::from_pretrained("TOKENIZER", None)?;
+    let tokenizer = Tokenizer::from_file("./hu/tokenizer.json")?;
+
+    let encoding = tokenizer.encode("안녕하세요 naver입니다", true)?;
+    println!("{:?}", encoding.get_tokens());
+    println!("{:?}", encoding.get_ids());
+    Ok(())
 }
 
 #[cfg(test)]
